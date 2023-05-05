61b50af0cba47989dc4b12f717220adca1bcfc9e
Yunzhe Wang <ywang393@jhu.edu>
2023-05-04 21:28:12 -0400

    Allow change of number of worker for loading data.

--- scripts/train_a_model.py
+++ scripts/train_a_model.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from xmot.mot.detectors import DNN
 import torch
 import click
-
+from pathlib import Path
 
 @click.command()
 @click.argument("dir1")
@@ -14,6 +14,8 @@ import click
 @click.option("--batch-size", default=2, type=int, help="Mini-batch size.")
 @click.option("--epoch", default=20, type=int, help="Number of epoches of training.")
 @click.option("--print-interval", default=200, type=int, help="Intervals to log values of loss functions.")
+@click.option("--num-workers", default=0, type=int, help="Number of processes to load training data." \
+              "It can be larger than batch_size. Each batch is assigned one process.")
 def train_a_model(dir1, size1, model_output, dir2, size2, batch_size, epoch, print_interval):
     """
     A script specially used for training models used to benchmark the effect of style transfered data
@@ -25,12 +27,12 @@ def train_a_model(dir1, size1, model_output, dir2, size2, batch_size, epoch, pri
     #print("Number of style training data:", len(dataloader)) # Print the number of iterations.
 
     model = DNN(device="cuda", model=None)
-    print(f"Training using data from {dir1}")
+    print(f"Training using data from {str(Path(dir1).resolve())}")
     model.train(dataloader, epoch=epoch, print_interval=print_interval)
 
     if size2 > 0 and dir2 is not None:
         print() # A new line
-        print(f"Training using data from {dir2}")
+        print(f"Training using data from {str(Path(dir2).resolve())}")
         dataset2 = BeadDatasetFile(dir2, len=size2)
         dataloader2 = DataLoader(dataset2, batch_size=batch_size, shuffle=True, collate_fn=collate_fn,
                                  num_workers=0, drop_last=False)
